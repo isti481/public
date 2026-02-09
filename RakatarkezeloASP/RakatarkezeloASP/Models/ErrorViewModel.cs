f9a077e2b42b04dd54bc1f0fa42d5f46d95d8ae8
isti481 <juhasz.istvan4@hotmail.com>
2026-02-09 14:57:21 +0100

    create project
    create project

---
+++ RakatarkezeloASP/RakatarkezeloASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RakatarkezeloASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
